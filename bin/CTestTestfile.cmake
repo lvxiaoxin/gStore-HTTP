8d2ee1b501faa38583a05c35db29157ddf307b8b
lvxiaoxin <lvxin1204@163.com>
2017-04-06 21:26:08 +0800

    clear

--- bin/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lvxin/Desktop/Simple-Web-Server
-# Build directory: /Users/lvxin/Desktop/Simple-Web-Server/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(tests)
--- bin/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lvxin/Desktop/Simple-Web-Server/tests
-# Build directory: /Users/lvxin/Desktop/Simple-Web-Server/bin/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(io_test "io_test")
-add_test(parse_test "parse_test")
-add_test(crypto_test "crypto_test")
